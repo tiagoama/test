b02bdde1605023eeff449d34bb80a066a2e3d3c8
tiagoama <tiagoamarilla35@gmail.com>
2023-03-03 11:54:59 -0300

    Añadi un puts en la linea 15

---
+++ testiño/src/testiño.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : testiño.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Esto es un test :)");
+	return EXIT_SUCCESS;
+
+}
