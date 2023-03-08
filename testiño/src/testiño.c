e0ba36a6903b212de79310ed30ca4e637b1d5ae6
tiagoama <tiagoamarilla35@gmail.com>
2023-03-08 11:59:05 -0300

    Ejercicio 1

--- testiño/src/testiño.c
+++ testiño/src/testiño.c
@@ -10,9 +10,33 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+int any(char s1[], char s2[]){
+	int i,j;
+	for(i=0; i<strlen(s2); i++){
+		for(j=0; j<strlen(s1); j++){
+			if (s1[j]==s2[i])
+				return j;
+		}
+	}
+	return -1;
+}
 
 int main(void) {
-	puts("Esto es un test :)");
-	return EXIT_SUCCESS;
+	char a[]="Pedro";
+	char b[]="o";
+	int x;
 
+	x = any(a,b);
+
+	if (x!=-1){
+		printf("La primera ocurrencia del caracter %c de s2 se dio en la posicion %d de s1",a[x],x);
+	}
+
+	else{
+		printf("No se encontro una ocurrencia");
+	}
+
+	return EXIT_SUCCESS;
 }
